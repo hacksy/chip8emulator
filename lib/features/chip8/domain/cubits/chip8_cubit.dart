4099e115d387f2d483c5cfa78ab29c26a6340544
hansyschmitt <55103581+hansyschmitt@users.noreply.github.com>
2024-05-01 13:12:49 -0500

    Add placeholder cubit

---
+++ lib/features/chip8/domain/cubits/chip8_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+part 'chip8_state.dart';
+
+class Chip8Cubit extends Cubit<Chip8State> {
+  Chip8Cubit(super.initialState);
+}
---
+++ lib/features/chip8/domain/cubits/chip8_state.dart
@@ -0,0 +1,6 @@
+part of 'chip8_cubit.dart';
+
+class Chip8State extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
